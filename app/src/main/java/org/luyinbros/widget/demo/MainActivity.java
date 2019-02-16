3c7c09f69bea883f669934fad03d92b368a5cfc7
Andy Hong <rainyeveningstreet@gmail.com>
2019-02-16 20:30:56 +0800

    Initial commit

---
+++ app/src/main/java/org/luyinbros/widget/demo/MainActivity.java
@@ -0,0 +1,15 @@
+package org.luyinbros.widget.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.luyinbros.widget.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
